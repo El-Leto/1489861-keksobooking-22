149df706be4fb9b0af2237e99294b4007f7a85ee
El-Leto <elleto.life@gmail.com>
2021-03-15 19:15:06 +0300

    Валидация формы

--- js/user-form.js
+++ js/user-form.js
@@ -3,6 +3,11 @@ const price = document.querySelector('#price');
 const timeIn = document.querySelector('#timein');
 const timeOut = document.querySelector('#timeout');
 const address = document.querySelector('#address');
+const userTitle = document.querySelector('#title');
+const userPrice = document.querySelector('#price');
+const roomNumberSelect = document.querySelector('#room_number');
+const capacitySelect = document.querySelector('#capacity');
+const MAX_ROOMS_NUMBER = 100;
 
 const PlaceTypes = {
   BUNGALO: 'bungalow',
@@ -46,5 +51,51 @@ timeIn.addEventListener('change', () => {
 timeOut.addEventListener('change', () => {
   timeIn.value = timeOut.value;
 });
+
+
+userTitle.addEventListener('invalid', () => {
+  if (userTitle.validity.tooShort) {
+    userTitle.setCustomValidity('Заголовок должен состоять минимум из 30 символов');
+  } else if (userTitle.validity.tooLong) {
+    userTitle.setCustomValidity('Заголовок не должен превышать 100 символов');
+  } else if (userTitle.validity.valueMissing) {
+    userTitle.setCustomValidity('Обязательное поле');
+  } else {
+    userTitle.setCustomValidity('');
+  }
+});
+
+userPrice.addEventListener('invalid', () => {
+  if (userPrice.validity.rangeOverflow) {
+    userPrice.setCustomValidity('Значение не должно превышать 1 000 000');
+  } else if (userPrice.validity.valueMissing) {
+    userPrice.setCustomValidity('Обязательное поле');
+  } else {
+    userPrice.setCustomValidity('');
+  }
+});
+
+const capacityCheck = () => {
+  const roomNumber = roomNumberSelect.value;
+  const capacity = capacitySelect.value;
+
+  if (roomNumber === MAX_ROOMS_NUMBER && capacity !== '0') {
+    capacitySelect.setCustomValidity('Выберите вариант "Не для гостей"');
+  } else if (roomNumber !== MAX_ROOMS_NUMBER && capacity === '0') {
+    capacitySelect.setCustomValidity('Выберите другой вариант');
+  } else if (roomNumber < capacity) {
+    capacitySelect.setCustomValidity('Выберите меньшее число гостей');
+  } else {
+    capacitySelect.setCustomValidity('');
+  }
+}
+
+capacitySelect.addEventListener('change', () => {
+  capacityCheck();
+})
+
+roomNumberSelect.addEventListener('change', () => {
+  capacityCheck();
+})
 
 export { setAddress };
